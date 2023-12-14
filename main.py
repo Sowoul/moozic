578f5a7945bda722ac28740854361d5b1f8e88c4
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-12-14 16:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
-from pytube import YouTube, Search
-from uuid import uuid1
+from pytube import Search
+from concurrent.futures import ThreadPoolExecutor
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+def grab_info(yt_obj):
+    audio_url = yt_obj.streams.filter(only_audio=True).first().url
+    return {
+        'thumbnail': yt_obj.thumbnail_url,
+        'title': yt_obj.title,
+        'views': yt_obj.views,
+        'creator': yt_obj.author,
+        'audio_url': audio_url,
+    }
 
 @app.route('/home')
 def run():
@@ -10,23 +22,24 @@ def run():
 
 @app.route('/', methods=["GET"])
 def moz():
-    def grab_info(yt_obj):
-        audio_url = (yt_obj.streams.filter(only_audio=True).first()).url
-        return {
-            'thumbnail': yt_obj.thumbnail_url,
-            'title': yt_obj.title,
-            'views': yt_obj.views,
-            'creator': yt_obj.author,
-            'audio_url': audio_url,
-        }
+    song = request.args.get('search')
+    video_info = []
 
-    song = request.args.get('search')   
-    
     if song:
-        global links
-        links = (Search(song).results)[:5]
-        video_info = [grab_info(video) for video in links]
-        return render_template('index.html', videos=video_info)
-    
+        cache_key = f'search_{song}'
+        cached_result = cache.get(cache_key)
+
+        if cached_result is not None:
+            video_info = cached_result
+        else:
+            links = (Search(song).results)[:5]
+
+            with ThreadPoolExecutor() as executor:
+                video_info = list(executor.map(grab_info, links))
+
+            cache.set(cache_key, video_info, timeout=60)
+
+    return render_template('index.html', videos=video_info)
+
 if __name__ == '__main__':
     app.run(port=8080)
